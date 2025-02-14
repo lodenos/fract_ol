88c5839be21c1d558ec31b52d60e3a1bca11415b
lodenos <nikolaiilodenos@gmail.com>
2025-02-14 16:00:31 +0000

    update diagram_move

--- src/diagram/diagram_mouve.c
+++ src/diagram/diagram_mouve.c
@@ -3,17 +3,11 @@
 // INFO: direction is a vector, every 1 is 100% size of map shift
 
 diagram_t* diagram_mouve(diagram_t* context, v2_f64_t* direction) {
-  v2_f64_t range;
-  v2_f64_t shift;
+  const v2_f64_t shift = (v2_f64_t){
+    .x = (context->boundary.bx - context->boundary.ax) * direction->x,
+    .y = (context->boundary.by - context->boundary.ay) * direction->y
+  };
 
-  range = (v2_f64_t){
-    .x = context->boundary.bx - context->boundary.ax,
-    .y = context->boundary.by - context->boundary.ay
-  };
-  shift = (v2_f64_t){
-    .x = range.x * direction->x,
-    .y = range.y * direction->y
-  };
   context->boundary = (v4_f64_t){
     .ax = context->boundary.ax + shift.x,
     .ay = context->boundary.ay + shift.y,
